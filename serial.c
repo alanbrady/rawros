0a0ee34d44981fdb95a1c26717cc2aca3e0ad5e0
alanbrady <a.brady.3@gmail.com>
2015-06-28 01:49:16 -0700

    Changed most of the serial CONSTANTS and more thoroughly implemented serial features

--- serial.c
+++ serial.c
@@ -2,9 +2,8 @@
 #include "io.h"
 
 void serial_write(char a) {
-    while(serial_is_transmit_empty(SERIAL_COM_BASE) == 0);
-
-    outb(SERIAL_COM_BASE, a);
+    while(serial_is_transmit_empty(SERIAL_COM1) == 0);
+    outb(SERIAL_COM1, a);
 }
 
 void serial_write_data(const char* str, const unsigned int len) {
@@ -15,30 +14,93 @@ void serial_write_data(const char* str, const unsigned int len) {
 }
 
 void serial_init(unsigned short com) {
-    serial_configure_baud_rate(com, 2);
+    serial_set_baud_rate(com, 115200);
     serial_configure_buffers(com);
-    serial_configure_line(com);
+    serial_configure_line(com, SERIAL_LCR_WORDLEN_8, SERIAL_LCR_NO_PARITY,
+            SERIAL_LCR_ONE_STOPBIT);
     serial_configure_modem(com);
 }
 
-void serial_configure_baud_rate(unsigned short com, unsigned short divisor) {
-    outb(SERIAL_LINE_COMMAND_PORT(com), SERIAL_LINE_ENABLE_DLAB);
-    outb(SERIAL_DATA_PORT(com), divisor & 0x00FF);
-    outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00FF);
+void serial_set_baud_rate(unsigned short com, unsigned int frequency) {
+    // old, probably garbage, left for reference until tested
+    /*outb(SERIAL_LINE_CONTROL_REG(com), SERIAL_LINE_ENABLE_DLAB);*/
+    /*outb(SERIAL_DATA_PORT(com), divisor & 0x00FF);*/
+    /*outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00FF);*/
+
+    // dlab changes the way base and base+1 behave
+    // with it enabled +0 and +1 are the divisor registers
+    serial_enable_dlab(com);
+
+    // calculate divisor based on the requested frequency
+    // frequency must be 0 > f >= 115200
+    // greater than 115200 caps the frequency and 0 makes default to
+    // 115200
+    if (frequency == 0 || frequency > 115200) frequency = 115200;
+    unsigned int divisor = 115200/frequency;
+
+    outb(SERIAL_DIV_LATCH_LOW(com), divisor & 0x00FF);
+    outb(SERIAL_DIV_LATCH_HIGH(com), (divisor >> 8) & 0x00FF);
+
+    // go ahead and disable dlab since it's only used when setting the
+    // divisor, this way dlab is isolated to this function
+    serial_disable_dlab(com);
 }
 
-void serial_configure_line(unsigned short com) {
-    outb(SERIAL_LINE_COMMAND_PORT(com), 0x03);
+int serial_configure_line(unsigned short com, unsigned short wordLength,
+        unsigned short parity, unsigned short stopBits) {
+    // check for invalid wordLength
+    if (wordLength < SERIAL_LCR_WORDLEN_5 || 
+            wordLength > SERIAL_LCR_WORDLEN_8) {
+        return 0;
+    }
+
+    // check for ivnalid parity
+    if (parity != SERIAL_LCR_NO_PARITY &&
+            parity != SERIAL_LCR_ODD_PARITY &&
+            parity != SERIAL_LCR_EVEN_PARITY &&
+            parity != SERIAL_LCR_MARK &&
+            parity != SERIAL_LCR_SPACE) {
+        return 0;
+    }
+
+    // check for invalid stop bit
+    if (stopBits != SERIAL_LCR_ONE_STOPBIT &&
+            stopBits != SERIAL_LCR_TWO_STOPBIT) {
+        return 0;
+    }
+
+    // good to go, setup our LCR with the given options
+    outb(SERIAL_LINE_CONTROL_REG(com), 
+            wordLength | parity | stopBits);
+    return -1;
 }
 
 void serial_configure_modem(unsigned short com) {
-    outb(SERIAL_MODEM_COMMAND_PORT(com), 0x03);
+    outb(SERIAL_MODEM_CONTROL_REG(com), 0x03);
 }
 
 void serial_configure_buffers(unsigned short com) {
-    outb(SERIAL_FIFO_COMMAND_PORT(com), 0xC7);
+    outb(SERIAL_FIFO_CONTROL_REG(com), 0xC7);
 }
 
 int serial_is_transmit_empty(unsigned short com) {
     return (inb(SERIAL_STATUS_PORT(com)) & 0x20);
+}
+
+void serial_enable_dlab(unsigned short com) {
+    short currLineReg = inb(SERIAL_LINE_CONTROL_REG(com));
+    outb(SERIAL_LINE_CONTROL_REG(com), currLineReg | 0x80);
+}
+
+void serial_disable_dlab(unsigned short com) {
+    short currLineReg = inb(SERIAL_LINE_CONTROL_REG(com));
+    outb(SERIAL_LINE_CONTROL_REG(com), currLineReg & 0x3F);
+}
+
+void serial_enable_rda_interrupt(unsigned short com) {
+    outb(SERIAL_INTERRUPT_ENABLE_REG(com), 0x01);
+}
+
+void serial_disable_rda_interrupt(unsigned short com) {
+    outb(SERIAL_INTERRUPT_ENABLE_REG(com), 0x00);
 }
--- serial.h
+++ serial.h
@@ -1,29 +1,62 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
-#define SERIAL_COM_BASE                     0x3F8
+// Com port address, BIOS may have different addresses for COM2+
+#define SERIAL_COM1                     0x3F8
+#define SERIAL_COM2                     0x2F8
+#define SERIAL_COM3                     0x3E8
+#define SERIAL_COM4                     0x2E8
 
-#define SERIAL_DATA_PORT(base)              (base)
-#define SERIAL_FIFO_COMMAND_PORT(base)      (base+2)
-#define SERIAL_LINE_COMMAND_PORT(base)      (base+3)
-#define SERIAL_MODEM_COMMAND_PORT(base)     (base+4) 
-#define SERIAL_STATUS_PORT(base)            (base+5) 
+#define SERIAL_DATA_TXRX(base)              (base)      // with DLAB off
+#define SERIAL_INTERRUPT_ENABLE_REG(base)   (base+1)
 
-#define SERIAL_LINE_ENABLE_DLAB             0x80
+#define SERIAL_DIV_LATCH_LOW(base)          (base)      // with DLAB on
+#define SERIAL_DIV_LATCH_HIGH(base)         (base+1)
+
+#define SERIAL_FIFO_CONTROL_REG(base)       (base+2)    // base+2 is different 
+#define SERIAL_INTERRUPT_ID_REG(base)       (base+2)    // depending on if r/w
+
+#define SERIAL_LINE_CONTROL_REG(base)       (base+3)
+#define SERIAL_MODEM_CONTROL_REG(base)      (base+4)
+#define SERIAL_STATUS_PORT(base)            (base+5)
+
+// Line Control Register Options
+#define SERIAL_LCR_WORDLEN_5                0x00
+#define SERIAL_LCR_WORDLEN_6                0x01
+#define SERIAL_LCR_WORDLEN_7                0x02
+#define SERIAL_LCR_WORDLEN_8                0x03
+
+#define SERIAL_LCR_ONE_STOPBIT              0X00
+#define SERIAL_LCR_TWO_STOPBIT              0X04
+
+#define SERIAL_LCR_NO_PARITY                0x00
+#define SERIAL_LCR_ODD_PARITY               0x08
+#define SERIAL_LCR_EVEN_PARITY              0x18
+#define SERIAL_LCR_MARK                     0x28
+#define SERIAL_LCR_SPACE                    0x38
+
+//#define SERIAL_LINE_ENABLE_DLAB             0x80
 
 void serial_write(char a);
-
 void serial_write_data(const char* str, const unsigned int len);
-
 void serial_init(unsigned short com);
 
-void serial_configure_baud_rate(unsigned short com, unsigned short divisor);
-
-void serial_configure_line(unsigned short com);
-
+/**
+  * serial_set_baud_rate
+  * com - com port to use
+  * frequency - serial connection frequency, 0 > f >= 115200
+  **/
+void serial_set_baud_rate(unsigned short com, unsigned int frequency);
+int serial_configure_line(unsigned short com, unsigned short wordLength,
+        unsigned short parity, unsigned short stopBits);
 void serial_configure_modem(unsigned short com);
-
 void serial_configure_buffers(unsigned short com);
+
+void serial_enable_dlab(unsigned short com);
+void serial_disable_dlab(unsigned short com);
+
+void serial_enable_rda_interrupt(unsigned short com);
+void serial_disable_rda_interrupt(unsigned short com);
 
 int serial_is_transmit_empty(unsigned short com);
 
