27ba4b136982041612c8e5bdb4e63c3064eb4755
alanbrady <a.brady.3@gmail.com>
2015-06-28 11:06:00 -0700

    Added comments for inb and fixed typo in outb

--- io.h
+++ io.h
@@ -6,10 +6,16 @@
  * Sends the given data to the given I/O port
  *
  * @param port The port to send the data
- * @param port The data to be sent
+ * @param data The data to be sent
  */
 extern void outb(const unsigned short port, const unsigned char data);
 
+/**
+ * inb:
+ * Recieves a byte of data from the given port
+ *
+ * @param port The port to receive data from
+ */
 extern unsigned char inb(const unsigned short port);
 
 #endif
