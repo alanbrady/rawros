f3760a5f181fd01991ace8bf15100480b28871a5
alanbrady <a.brady.3@gmail.com>
2015-07-29 20:59:39 -0700

    Experimenting with va_list

--- utils.c
+++ utils.c
@@ -11,6 +11,66 @@ unsigned char isCom1Init = 0;
 unsigned char isCom2Init = 0;
 
 void printk(const unsigned short out, const char* fmt, ...) {
+    va_list vl;
+    va_start(vl, fmt);
+    /*int val = va_arg(vl, int);*/
+    int val = (*(int*)vl);
+    int i = 0;
+    for (; i < 8; ++i) {
+        switch ((val >> (7-i)) & 0x000F) {
+            case 0x0:
+                fb_write("0", 1);
+                break;
+            case 0x1:
+                fb_write("1", 1);
+                break;
+            case 0x2:
+                fb_write("2", 1);
+                break;
+            case 0x3:
+                fb_write("3", 1);
+                break;
+            case 0x4:
+                fb_write("4", 1);
+                break;
+            case 0x5:
+                fb_write("5", 1);
+                break;
+            case 0x6:
+                fb_write("6", 1);
+                break;
+            case 0x7:
+                fb_write("7", 1);
+                break;
+            case 0x8:
+                fb_write("8", 1);
+                break;
+            case 0x9:
+                fb_write("9", 1);
+                break;
+            case 0xa:
+                fb_write("a", 1);
+                break;
+            case 0xb:
+                fb_write("b", 1);
+                break;
+            case 0xc:
+                fb_write("c", 1);
+                break;
+            case 0xd:
+                fb_write("d", 1);
+                break;
+            case 0xe:
+                fb_write("e", 1);
+                break;
+            case 0xf:
+                fb_write("f", 1);
+                break;
+        }
+        /*val = val >> 1;*/
+    }
+    /*va_end;*/
+
     unsigned int len = strlen(fmt);
     switch(out) {
         case PRINTK_FB:
