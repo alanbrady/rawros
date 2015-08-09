d8c459019ca6c6b2138cfee223ffd29c748c6f10
alanbrady <a.brady.3@gmail.com>
2015-08-08 18:20:32 -0700

    Implement printing interrupt err_code

--- isr.c
+++ isr.c
@@ -3,6 +3,6 @@
 #include "utils.h"
 
 void isr_handler(registers_t regs) {
-    printk(PRINTK_FB, "received interrupt");
+    printk(PRINTK_FB, "interrupt: %h\n", regs.err_code);
     (void)regs;
 }
