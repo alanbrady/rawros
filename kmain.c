1a2337bbf0228ec1f3b0d678722d0247578647ff
alanbrady <a.brady.3@gmail.com>
2015-08-09 13:18:10 -0700

    Refactor memory printing
    The memory printing in kmain was a bit ugly and was something relatively
    standard that could benefit from being encapsulated into a function.

--- kmain.c
+++ kmain.c
@@ -2,6 +2,9 @@
 #include "memory.h"
 #include "gdt.h"
 #include "idt.h"
+#include "string.h"
+
+static void print_memory_info(multiboot_info_t* mbi);
 
 // main function loaded through loader.asm
 int kmain(multiboot_info_t* mbi, uint32_t magic) {
@@ -11,15 +14,7 @@ int kmain(multiboot_info_t* mbi, uint32_t magic) {
     idt_init();
     clrscr();
 
-    multiboot_memory_map_t* mmap = (multiboot_memory_map_t*)mbi->mmap_addr;
-    unsigned int mem_index = 0;
-    while ((uint32_t)mmap < (mbi->mmap_addr + mbi->mmap_length)) {
-        printk(PRINTK_FB, "memory %u: type:%u base: %H length: %H\n",
-                mem_index++, mmap->type, mmap->base_addr, mmap->length);
-
-        mmap = (multiboot_memory_map_t*) ((uint32_t)mmap + mmap->size +
-                sizeof(uint32_t));
-    }
+    print_memory_info(mbi);
 
     printk(PRINTK_FB, "bitsize - char:  %u\n", sizeof(char)*8);
     printk(PRINTK_FB, "bitsize - short: %u\n", sizeof(short)*8);
@@ -30,4 +25,27 @@ int kmain(multiboot_info_t* mbi, uint32_t magic) {
     asm volatile("int $0x4");
 
     return 0;
+}
+
+static void print_memory_info(multiboot_info_t* mbi) {
+    multiboot_memory_map_t* mmap = (multiboot_memory_map_t*)mbi->mmap_addr;
+    unsigned int mem_index = 0;
+    const char* headerStr = "| ID | Type | base               | \
+length             |\n";
+    const unsigned int headerLen = strlen(headerStr);
+    unsigned int i;
+
+    printk(PRINTK_FB, "Memory:\n");
+    printk(PRINTK_FB, headerStr);
+    for (i = 0; i < headerLen; ++i) {
+        printk(PRINTK_FB, "-");
+    }
+    printk(PRINTK_FB, "\n");
+    while ((uint32_t)mmap < (mbi->mmap_addr + mbi->mmap_length)) {
+        printk(PRINTK_FB, "| %u  | %u    | %H | %H |\n",
+                mem_index++, mmap->type, mmap->base_addr, mmap->length);
+
+        mmap = (multiboot_memory_map_t*) ((uint32_t)mmap + mmap->size +
+                sizeof(uint32_t));
+    }
 }
