acca17ff8c3769621c7a4487926e24fff768e6f6
alanbrady <a.brady.3@gmail.com>
2015-08-09 13:27:01 -0700

    Move framebuffer comments to header

--- framebuffer.c
+++ framebuffer.c
@@ -3,32 +3,12 @@
 
 char* fb = (char*)0x000b8000; // frame buffer/address
 
-/**
- * fb_write_cell
- *  Write the given data to a given cell with the given
- *  foreground and background colors
- *
- *  @param cell Cell to write to
- *  @param c Character to write
- *  @param fg Foreground color
- *  @param bg Background color
- *  **/
 void fb_write_cell(const unsigned int cell, char c, 
         unsigned char fg, unsigned char bg) {
     fb[cell] = c;
     fb[cell+1] = ((bg & 0x0F) << 4) | (fg & 0x0F);
 }
 
-/**
- * fb_write_cell_string
- *  Write the given string to a given cell with the given
- *  foreground and background colors
- *
- *  @param cell Cell to write to
- *  @param str String to write, must be null terminated
- *  @param fg Foreground color
- *  @param bg Background color
- *  **/
 void fb_write_cell_string(const unsigned int cell, const char* str, 
         unsigned char fg, unsigned char bg) {
     unsigned int i = cell;
@@ -39,17 +19,6 @@ void fb_write_cell_string(const unsigned int cell, const char* str,
     }
 }
 
-/**
- * fb_write_cell_data
- *  Write the given data array to a given cell with the given
- *  foreground and background colors with the given length
- *
- *  @param cell Cell to write to
- *  @param data Data to write to frame buffer
- *  @param len Length of data to write from data array
- *  @param fg Foreground color
- *  @param bg Background color
- *  **/
 void fb_write_cell_data(const unsigned int cell, 
         const char* data, const unsigned int len, 
         unsigned char fg, unsigned char bg) {
--- framebuffer.h
+++ framebuffer.h
@@ -27,12 +27,43 @@
 #define FB_HIGH_BYTE_COMM   14
 #define FB_LOW_BYTE_COMM    15
 
+/**
+ * fb_write_cell
+ *  Write the given data to a given cell with the given
+ *  foreground and background colors
+ *
+ *  @param cell Cell to write to
+ *  @param c Character to write
+ *  @param fg Foreground color
+ *  @param bg Background color
+ *  **/
 void fb_write_cell(const unsigned int cell, char c, 
         unsigned char fg, unsigned char bg);
 
+/**
+ * fb_write_cell_string
+ *  Write the given string to a given cell with the given
+ *  foreground and background colors
+ *
+ *  @param cell Cell to write to
+ *  @param str String to write, must be null terminated
+ *  @param fg Foreground color
+ *  @param bg Background color
+ *  **/
 void fb_write_cell_string(const unsigned int cell, const char* str, 
         unsigned char fg, unsigned char bg);
 
+/**
+ * fb_write_cell_data
+ *  Write the given data array to a given cell with the given
+ *  foreground and background colors with the given length
+ *
+ *  @param cell Cell to write to
+ *  @param data Data to write to frame buffer
+ *  @param len Length of data to write from data array
+ *  @param fg Foreground color
+ *  @param bg Background color
+ *  **/
 void fb_write_cell_data(const unsigned int cell, const char* data, 
         const unsigned int len, unsigned char fg, unsigned char bg); 
 
